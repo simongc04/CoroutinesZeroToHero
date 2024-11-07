6cd2003b6004fb7ad1eddd8eb8f6ac22216778d8
simon <simonescuela9@gmail.com>
2024-11-07 09:53:20 +0000

    añadi dentro de el @get la clase albums

--- app/src/main/java/com/example/coroutineszerotohero/ApiServices.kt
+++ app/src/main/java/com/example/coroutineszerotohero/ApiServices.kt
@@ -1,18 +1,10 @@
+// ApiServices.kt
 package com.example.coroutineszerotohero
 
-
-
-
-
-import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiServices {
-
-    @GET("/api/a83e4dcabe9e4d1014d42c002c3f977b/search/{name}")
-   suspend fun getSuperheroes(@Path("name") superheroName:String): Response<SuperHeroDataResponse>
+    @GET("albums")
+    suspend fun getAlbums(): Response<List<Album>>
 }
-
-data class SuperHeroDataResponse(@SerializedName("response") val response:String)
