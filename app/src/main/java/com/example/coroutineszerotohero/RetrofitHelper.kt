208bf573bb5a3181aa65eb115be2472248ea9cf7
simon <simonescuela9@gmail.com>
2024-11-07 09:55:58 +0000

    codigo adaptado para jsonplaceholder

--- app/src/main/java/com/example/coroutineszerotohero/RetrofitHelper.kt
+++ app/src/main/java/com/example/coroutineszerotohero/RetrofitHelper.kt
@@ -1,15 +1,20 @@
+// RetrofitHelper.kt
 package com.example.coroutineszerotohero
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    private const val URL = "https://superheroapi.com/"
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
-    private val retrofit =
-        Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
-    fun getInstance():ApiServices{
-        return retrofit.create(ApiServices::class.java)
+    val apiService: ApiServices by lazy {
+        retrofit.create(ApiServices::class.java)
     }
 }
